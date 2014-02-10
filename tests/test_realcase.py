26b2fa8d377b29d5b690d4f3001aa112acddf960
Huang Hao <hao.h.huang@intel.com>
2014-02-10 11:27:37 +0800

    Add more tests.
    * runtest should exit with non-zero if any case failed
    * runtest exit with zero if multi cases are all passed
    
    Change-Id: Ic6dfaac6694fc5f46d6b86e3e618ee7bb38c5e9b

--- tests/test_realcase.py
+++ tests/test_realcase.py
@@ -4,7 +4,9 @@ import subprocess
 
 
 class RealCaseTest(unittest.TestCase):
-    pass
+    """
+    Test methods will be loaded at runtime
+    """
 
 
 def _popen(cmd):
@@ -18,31 +20,42 @@ STDERR:
 %s""" % (proc.returncode, stdout, stderr)
     return proc.returncode, msg
 
-def _create_test(name):
+def _create_test(case_names, expect_pass=True, method_name=None):
     path = os.path.join(os.path.dirname(__file__), 'cases')
-    funcname = 'test_%s' % name.replace('.', '_')
+    if not method_name:
+        method_name = 'test_%s' % case_names[0].replace('.', '_')
+
     def test(self):
-        cmd = "cd %s; runtest -vv %s" % (path, name)
+        cmd = "cd %s; runtest -vv %s" % (path, ' '.join(case_names))
         ret, msg = _popen(cmd)
-        self.assertEqual(0, ret, msg)
-    setattr(RealCaseTest, funcname, test)
+        if expect_pass:
+            self.assertEqual(0, ret, msg)
+        else:
+            self.assertNotEqual(0, ret, msg)
+
+    test.__name__ = method_name
+    setattr(RealCaseTest, method_name, test)
 
 def _create_test_in_tproj(name):
     path = os.path.join(os.path.dirname(__file__), 'tproj', 'cases')
     funcname = 'test_tproj_%s' % name.replace('.', '_')
     def test(self):
         cmd = "cd %s; runtest -vv %s" % (path, name)
-        print cmd
         ret, msg = _popen(cmd)
         self.assertEquals(0, ret, msg)
     setattr(RealCaseTest, funcname, test)
 
 
 def _load_tests():
-    _create_test('simple.xml')
-    _create_test('cdata.xml')
-    _create_test('qa.xml')
-    _create_test('content_fixture.xml')
+    _create_test(['simple.xml'])
+    _create_test(['simple_false.xml'], False)
+    _create_test(['cdata.xml'])
+    _create_test(['qa.xml'])
+    _create_test(['content_fixture.xml'])
+    _create_test(['simple.xml', 'cdata.xml'],
+        method_name="test_multi_case_pass")
+    _create_test(['simple.xml', 'simple_false.xml'], False,
+        method_name="test_multi_case_failed")
 
     _create_test_in_tproj('copy_fixture.xml')
     _create_test_in_tproj('template_fixture.xml')
